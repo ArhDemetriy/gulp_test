64b5a04ec32c4c7f9d17ee58c51aeab23c883a8f
ArhDemetriy <ArhDemetriy@GMail.com>
2022-12-09 20:42:22 +0800

    dev

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,14 +1,25 @@
-// const Gulp = require("gulp");
 import * as Gulp from "gulp"
+import * as Fs from "fs"
 
 const enum PATHS {
     jsons = "src/*.json"
 }
 
-const q = Gulp.task('json', function (this: any, done) {
-    console.log('task', 'this', this)
-    PATHS.jsons
+const returns = Gulp.task('default', function (done) {
+    const src = Gulp.dest('dist')
+
+    Gulp.src(PATHS.jsons)
+        // .pipe(Fs.createWriteStream(''))
+        .pipe(Gulp.dest('dist'))
+
+    const result: { [k in string]: any } = {}
+    for (const [key, value] of Object.entries(src))
+        result[key] = (typeof value === 'function') ? value.toString() : value
+
+    Fs.writeFileSync('srcConsist.json', JSON.stringify(result, undefined, 4))
+
     done()
 })
 
-console.log('Gulp', 'task return', q)
+// console.log('createWriteStream', Gulp.dest('dist'))
+// console.log('task returns', returns)
